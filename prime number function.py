a3cb6d85df6f14b38c6ebb8af5a6f7551d0b96ee
hamed gh <gharghi9@gmail.com>
2024-07-26 09:33:32 +0330

    change to helloworld

--- prime number function.py
+++ prime number function.py
@@ -1,10 +1,1 @@
-def prime(x):
-    z = 0
-    for i in range(1, x + 1):
-        if x % i == 0:
-            z += 1
-    if z == 2:
-        return True
-    else:
-        return False
-# x must be an integer and if the number was prime it return True otherwise it return False
+print("hello world")
